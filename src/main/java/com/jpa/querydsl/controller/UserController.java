ccaa6470fa817e2c92fcc4b1a24e255539ef3289
amanxu@126.com <amanxu@126.com>
2020-02-28 09:15:44 +0800

    添加单表新增和更新逻辑

--- src/main/java/com/jpa/querydsl/controller/UserController.java
+++ src/main/java/com/jpa/querydsl/controller/UserController.java
@@ -2,16 +2,20 @@ package com.jpa.querydsl.controller;
 
 import com.jpa.querydsl.model.QUserBean;
 import com.jpa.querydsl.model.UserBean;
+import com.jpa.querydsl.model.dto.UserCreateDTO;
 import com.jpa.querydsl.service.UserJPA;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -26,7 +30,7 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserJPA usrJPA;
+    private UserJPA userJPA;
 
     @Autowired
     private EntityManager entityManager;
@@ -41,6 +45,37 @@ public class UserController {
     public void initFactory() {
         queryFactory = new JPAQueryFactory(entityManager);
         log.debug("queryFactory init success");
+    }
+
+    @ApiOperation(value = "新增用户")
+    @PostMapping(value = "/create")
+    public UserBean createUser(@RequestBody UserCreateDTO userCreateDTO) {
+        UserBean userBean = new UserBean();
+        // 模拟对象转换
+        BeanUtils.copyProperties(userCreateDTO, userBean);
+
+        // 持久化用户数据
+        UserBean saveResult = userJPA.save(userBean);
+        return saveResult;
+    }
+
+    @Transactional
+    @ApiOperation(value = "新增用户")
+    @PostMapping(value = "/update")
+    public Long updateUser(@RequestBody UserCreateDTO userCreateDTO) {
+
+        Assert.notNull(userCreateDTO.getId(), "update id is null");
+        QUserBean qUserBean = QUserBean.userBean;
+
+        // 持久化用户数据
+        long updateResult = queryFactory.update(qUserBean)
+                .where(qUserBean.id.eq(userCreateDTO.getId()))
+                .set(qUserBean.name, userCreateDTO.getName())
+                .set(qUserBean.age, userCreateDTO.getAge())
+                .set(qUserBean.address, userCreateDTO.getAddress())
+                .set(qUserBean.pwd,userCreateDTO.getPwd())
+                .execute();
+        return updateResult;
     }
 
 
