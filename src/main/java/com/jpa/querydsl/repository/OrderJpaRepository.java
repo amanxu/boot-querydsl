58156f4860bc00b0bc429334a764f7242f4770a9
amanxu@126.com <amanxu@126.com>
2020-03-03 15:31:35 +0800

    新增JPA查询接口

--- src/main/java/com/jpa/querydsl/repository/OrderJpaRepository.java
+++ src/main/java/com/jpa/querydsl/repository/OrderJpaRepository.java
@@ -2,11 +2,22 @@ package com.jpa.querydsl.repository;
 
 import com.jpa.querydsl.model.UserOrderBean;
 
+import java.util.List;
+
 /**
  * @description:
  * @author: niexiaoxu
  * @date: 2020/2/29 1:35 PM
  */
 public interface OrderJpaRepository extends BaseJpaRepository<UserOrderBean> {
+
+
+    /**
+     * 根据用户ID和商品ID查询
+     * @param userId
+     * @param productId
+     * @return
+     */
+    List<UserOrderBean> findByUserIdAndProductId(Long userId, Long productId);
 
 }
